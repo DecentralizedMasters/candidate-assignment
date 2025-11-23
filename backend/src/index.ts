f79f25d2817937ea5caa67175ae250934637c6e2
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-11-24 01:31:10 +0200

    Add CORS support and update package backend dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import { ethers } from 'ethers';
 import express from 'express'
+import cors from 'cors';
+
 
 
 const PORT = process.env.PORT || 3000
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 
