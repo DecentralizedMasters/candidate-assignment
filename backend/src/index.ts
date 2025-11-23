a78d7f7de1518c8e8899785f26c6f3356773dfea
VolodymyrMikhno <volodymyr3583@gmail.com>
2025-11-24 00:10:45 +0200

    Implement initial server setup with signature verification and history

---
+++ backend/src/index.ts
@@ -0,0 +1,45 @@
+import { ethers } from 'ethers';
+import express from 'express'
+
+
+const PORT = process.env.PORT || 3000
+const app = express()
+app.use(express.json())
+
+
+type HistoryEntry = {
+  originalMessage: string, signer: string, timestamp: number, isValid: boolean
+}
+
+const history: Array<HistoryEntry> = [];
+app.get('/history', (_, res) => {
+  res.json(history)
+})
+
+
+app.post("/verify-signature", (req, res) => {
+  let isValid = false, signer = "";
+  const { message, signature } = req.body ?? {};
+  if (!message || !signature) {
+    return res.status(400).json({ error: "Missing message or signature" });
+  }
+  try {
+    signer = ethers.verifyMessage(message, signature);
+    isValid = !!signer;
+  } catch (error) {
+
+  }
+  const entry: HistoryEntry = {
+    originalMessage: message,
+    signer,
+    timestamp: Date.now(),
+    isValid
+  }
+  history.push(entry)
+  res.json({ isValid, signer, originalMessage: message });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})  
